b133839c2fbc1763b8227d00d3d840dfce1561c4
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-10-03 20:08:49 -0400

    .pre-commit-run.sh: use $LOG thruout

--- .pre-commit-run.sh
+++ .pre-commit-run.sh
@@ -3,13 +3,15 @@
 # invoked from .pre-commit-config.yaml to run mypy
 LOG=$0.log
 date > $LOG
-echo -- $0 $* >> $LOG
-#env >> /tmp/xxx.log
+echo $0 $* >> $LOG
+#env >> $LOG
+
+# saved copy of pyproject.toml:
 TMP=$0.tmp
 if ! cmp -s pyproject.toml $TMP; then
     pip install '.[mypy]'
     cp -p pyproject.toml $TMP
 fi
-#pip list >> /tmp/xxx.log
+#pip list >> $LOG
 # NOTE! first arg must be command to invoke!
 "$@"
