4d960ade19dd68ad27df063c9fa3cd418b23f099
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-10-03 22:24:14 -0400

    .pre-commit-run.sh: stash pyproject.toml file in VIRTUAL_ENV, if set

--- .pre-commit-run.sh
+++ .pre-commit-run.sh
@@ -4,10 +4,19 @@
 LOG=$0.log
 date > $LOG
 echo $0 $* >> $LOG
-#env >> $LOG
+env >> $LOG
+
+# want to stash copy of pyproject.toml near pre-commit created virtual environment
+if [ -n "$VIRTUAL_ENV" ]; then
+    TMP=$VIRTUAL_ENV/pyproject.toml
+else
+    # try "which python3"? grab first PATH element??
+    # hidden from view
+    TMP=$0.tmp
+fi
+echo TMP $TMP >> $LOG
 
 # saved copy of pyproject.toml:
-TMP=$0.tmp
 if ! cmp -s pyproject.toml $TMP; then
     pip install '.[mypy]'
     cp -p pyproject.toml $TMP
