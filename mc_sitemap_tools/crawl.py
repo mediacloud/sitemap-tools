1a017edb13cc711b002c5ab85b07a51329f711fa
Phil Budne <phil@regressive.org>
2026-01-30 13:16:48 -0500

    mc_sitemap_tools/crawl.py: add pruning/heuristics
    Experiment to see if one-level crawl is sufficient!
    Refactored Crawler into BaseCrawler, GNewsCrawler[Full]
    
    Bumps version to 3.0, since crawl.py API has changed

--- mc_sitemap_tools/crawl.py
+++ mc_sitemap_tools/crawl.py
@@ -1,14 +1,32 @@
 """
-tools for performing full crawls of a site
+tools for performing crawls of sitemap files
+
+Created in 2024, performing full site crawls. Mashed in Jan 2026 to do
+more limited crawling, adding depth and preference/weight to the work
+queue, and adding max_results to allow quitting early to see if we can
+do better than discover.NewsDiscoverer.find_gnews_fast in reasonable time.
+
+Author's note:
+
+This is a work in progress; I've flipped back and forth between the
+ordering of depth and pref in the PageTuple (and hence the work queue
+ordering).
+
+The page "preference" value is serving double duty; for lower values
+it controls which pages are visited first, values at/above DISCARD
+remove the URL from consideration (prune the tree).
 """
 
+import heapq
 import logging
+import re
 import time
-from typing import Callable, NamedTuple, cast
+from enum import Enum
+from typing import NamedTuple, cast
+from urllib.parse import urlsplit
 
 # PyPI
 from mcmetadata.feeds import normalize_url
-from mcmetadata.webpages import MEDIA_CLOUD_USER_AGENT
 from requests.exceptions import RequestException
 
 # local package
@@ -17,10 +35,23 @@ from . import discover, parser
 logger = logging.getLogger(__name__)
 
 
-Saver = Callable[[parser.Urlset], None]
+class PageTuple(NamedTuple):  # for heap
+    pref: float  # for sort (lowest first)
+    depth: int  # for sort
+    url: str
+    origin: str  # discovery path
 
 
-class Crawler:
+class VisitResult(Enum):
+    DONE = "done"
+    MORE = "more"
+    FIN = "fin"
+
+
+DISCARD = 10
+
+
+class BaseCrawler:
     """
     enscapsulate state for crawling a site.
     meant to be pickleable (no open files/sessions)
@@ -29,35 +60,59 @@ class Crawler:
     """
 
     FETCH_EXCEPTIONS = (RequestException,)
+    UNPUBLISHED_INDEX_PREF = 1  # after robots.txt
+    TRY_UNPUBLISHED_INDEX_PATHS = False
+    DISCARD_PREF = DISCARD
+    UNPUB_DEPTH = 1  # 0 to prefer to files in robots.txt
 
-    def __init__(self, home_page: str, saver: Saver, user_agent: str):
+    def __init__(
+        self, home_page: str, user_agent: str, max_depth: int, max_results: int
+    ):
         if not home_page.endswith("/"):
             home_page += "/"
         self.home_page = home_page
-        self.saver = saver
         self.user_agent = user_agent
+        self.max_depth = max_depth
+        self.max_results = max_results
 
+        self.results: list[parser.Urlset] = []
         self.news_discoverer = discover.NewsDiscoverer(user_agent)
-        self.to_visit: list[str] = []
+        self.to_visit: list[PageTuple] = []  # managed by heapq
         self.seen: set[str] = set()
-        self.state_processor = None
-        self.get_robots = True
+        self.get_robots = True  # starting state
+        self.robots_urls: list[str] = []
+        self.pages_visited = 0
 
-    def _add_url(self, url: str, home_page: str | None = None) -> None:
-        """
-        `url` may be complete URL (if `home_page` is None),
-        or path to append to `home_page`
-        checks if already seen before adding to `to_visit` list
-        """
-        if home_page:
-            url = home_page + url
-        canurl = normalize_url(url)
-        if canurl not in self.seen:
-            logger.info("adding %s", url)
-            self.seen.add(canurl)
-            self.to_visit.append(url)
+    def page_pref(self, url: str) -> float:
+        return 0.0
 
-    def _add_list(self, url_list: list[str], add_home_page: bool) -> None:
+    def _add_url(
+        self, depth: int, url: str, origin: str, pref: float | None = None
+    ) -> None:
+        """
+        `url` MUST be complete URL
+        checks if already seen before adding to `to_visit` heapq
+        """
+        nurl = normalize_url(url)
+        if nurl not in self.seen:
+            self.seen.add(nurl)
+            if pref is None:
+                pref = self.page_pref(url)
+                if pref >= self.DISCARD_PREF:
+                    logger.info("skipping %s %d %g %s", url, depth, pref, origin)
+                else:
+                    logger.info("adding %s %g %d %s", url, pref, depth, origin)
+                    page = PageTuple(url=url, depth=depth, pref=pref, origin=origin)
+                    heapq.heappush(self.to_visit, page)
+
+    def _add_list(
+        self,
+        depth: int,
+        url_list: list[str],
+        add_home_page: bool,
+        origin: str,
+        pref: float | None = None,
+    ) -> None:
         """
         add list of urls to visit
         if `add_home_page` is True, prepend `home_page` to each url
@@ -65,31 +120,52 @@ class Crawler:
         for url in url_list:
             if add_home_page:
                 url = self.home_page + url
-            self._add_url(url)
+            self._add_url(depth, url, pref=pref, origin=origin)
 
-    def visit_one(self) -> bool:
+    def add_unpublished_paths(self) -> None:
+        if self.TRY_UNPUBLISHED_INDEX_PATHS:
+            logger.info("====> add UNPUBLISHED_SITEMAP_INDEX_PATHS")
+            self._add_list(
+                depth=self.UNPUB_DEPTH,
+                url_list=discover._UNPUBLISHED_SITEMAP_INDEX_PATHS,
+                add_home_page=True,
+                pref=self.UNPUBLISHED_INDEX_PREF + 0.1,  # worse than organic
+                origin="unpub-index-paths",
+            )
+
+    def visit_one(self) -> VisitResult:
         """
         visit one page, returns True while more work to be done
+        to allow running as a background activity
         """
-        if self.get_robots:
-            # initial state: seed visit_list with pages in robots.txt
+        self.pages_visited += 1
+        if self.get_robots:  # starting?
+            # initial state: seed visit list with pages in robots.txt
             # and "well known" paths
-            logger.info("getting robots.txt")
+            logger.info("===== getting robots.txt")
             try:
+                self.robots_urls = self.news_discoverer.robots_sitemaps(self.home_page)
+                # XXX get/honor Crawl-Delay??
+                # sitemap lines in robots.txt _should_ be complete urls!!
                 self._add_list(
-                    self.news_discoverer.robots_sitemaps(self.home_page), False
+                    depth=0,
+                    url_list=self.robots_urls,
+                    add_home_page=False,
+                    origin="robots.txt",
                 )
             except self.FETCH_EXCEPTIONS:
                 pass
-            self._add_list(discover._UNPUBLISHED_SITEMAP_INDEX_PATHS, True)
-            self._add_list(discover._UNPUBLISHED_GNEWS_SITEMAP_PATHS, True)
 
-            self.get_robots = False
+            # maybe only add these AFTER robots.txt paths exhausted?
+            self.add_unpublished_paths()
+
+            self.get_robots = False  # initialized
             # robots.txt counts as a visit
             # so don't visit any pages
         else:
-            url = self.to_visit.pop(0)
-            logger.info("getting %s", url)
+            page = heapq.heappop(self.to_visit)
+            url = page.url
+            logger.info("getting %s %g %d %s", url, page.pref, page.depth, page.origin)
             # fetch and parse page:
             try:
                 sitemap = self.news_discoverer.sitemap_get(url)
@@ -101,93 +177,183 @@ class Crawler:
                 if smt == "index":
                     logger.info("%s: index", url)
                     index = cast(parser.Index, sitemap)
-                    for suburl in index["sub_sitemap_urls"]:
-                        self._add_url(suburl)
+                    if page.depth + 1 <= self.max_depth:
+                        for suburl in index["sub_sitemap_urls"]:
+                            self._add_url(
+                                page.depth + 1, suburl, origin=f"{page.origin} -> {url}"
+                            )
+                    else:
+                        logger.info("%s too deep", url)
                 elif smt == "urlset":
                     logger.info("%s: urlset", url)
                     urlset = cast(parser.Urlset, sitemap)
-                    self.saver(urlset)
+                    if self.save(urlset, page.pref):
+                        return VisitResult.DONE
                 else:
                     logger.warning("%s: unknown sitemap type %s", url, smt)
             # end not _get_robots
-        return len(self.to_visit) > 0
+
+        if len(self.to_visit) > 0:
+            return VisitResult.MORE
+        return VisitResult.FIN
+
+    def save(self, urls: parser.Urlset, pref: float) -> bool:
+        """
+        return True to cause visit_one to return DONE
+        """
+        logger.info("saving %s %g", urls["url"], pref)
+        self.results.append(urls)
+        return len(self.results) >= self.max_results
 
 
-def full_crawl_gnews_urls(home_page: str, sleep_time: float = 1.0) -> list[str]:
+class GNewsCrawlerFull(BaseCrawler):
     """
-    Returns list of sitemap urlsets with google_news_tags.
-
-    If you're spending the time to do a full crawl, you might consider
-    full_crawl_urlsets, which returns all urlsets along with
-    gnewsiness and number of entries!
+    crawl full site, returning only google news sitemaps,
+    without pruning, for time/quality comparison to pruned/heuristic version.
     """
-    results = []
 
-    def saver(urlset: parser.Urlset) -> None:
-        if urlset["google_news_tags"]:
-            url = urlset["url"]
-            logger.info("*** SAVING %s ***", url)
-            results.append(url)
+    UNPUBLISHED_GNEWS_SITEMAP_PREF = 0.0
 
-    crawler = Crawler(home_page, saver, MEDIA_CLOUD_USER_AGENT)
-    while crawler.visit_one():
-        logger.info("sleeping %.3f", sleep_time)
-        time.sleep(sleep_time)
-
-    return results
-
-
-class UrlsetInfo(NamedTuple):
-    url: str
-    size: int  # page size in characters
-    gnews: bool
-    entries: int
-    lastlastmod: str | None
-
-
-def full_crawl_urlsets(home_page: str, sleep_time: float = 1.0) -> list[UrlsetInfo]:
-    """
-    Returns list of sitemap urlset summaries using Crawler class
-    """
-    results = []
-
-    def saver(urlset: parser.Urlset) -> None:
-
-        url = urlset["url"]
-        lastlastmod = ""
-        for pg in urlset["pages"]:
-            lm = pg.get("lastmod")
-            if lm and lm > lastlastmod:
-                lastlastmod = lm
-
-        ui = UrlsetInfo(
-            url=url,
-            size=urlset["size"],
-            gnews=urlset["google_news_tags"],
-            entries=len(urlset["pages"]),
-            lastlastmod=lastlastmod or None,
+    def add_unpublished_paths(self) -> None:
+        logger.info("====> add UNPUBLISHED_GNEWS_SITEMAP_PATHS")
+        self._add_list(
+            depth=self.UNPUB_DEPTH,
+            url_list=discover._UNPUBLISHED_GNEWS_SITEMAP_PATHS,
+            add_home_page=True,
+            pref=self.UNPUBLISHED_GNEWS_SITEMAP_PREF + 0.1,  # worse than organic
+            origin="unpub-gnews-paths",
         )
-        logger.info("*** SAVING %s", ui)
-        results.append(ui)
+        super().add_unpublished_paths()
 
-    crawler = Crawler(home_page, saver, MEDIA_CLOUD_USER_AGENT)
-    while crawler.visit_one():
-        time.sleep(sleep_time)
+    def save(self, urls: parser.Urlset, pref: float) -> bool:
+        """
+        return True to cause visit_one to return DONE
+        """
+        if urls["google_news_tags"]:
+            return super().save(urls, pref)
+        return False
 
-    return results
+
+class GNewsCrawler(GNewsCrawlerFull):
+    """
+    google news sitemap specific site crawler,
+    with heuristics/pruning
+    """
+
+    # DATE_RE and YEAR_RE are class members to allow override;
+    # applied to case-flattened {path}?{query}
+    # matching either causes page_pref to return DISCARD.
+
+    # https://www.nytimes.com/sitemaps/new/cooking-1982-09.xml.gz
+    # https://nypost.com/sitemap-1865.xml
+    # https://nypost.com/sitemap-1999.xml?mm=12&dd=31
+    YEAR_RE = re.compile(r"(^|\D)(1[89]|20)\d\d($|\D|[01]\d)|year=")
+
+    # npr.org has dd-Mon-yy, bershireeagle has date=YYYY-MM-DD
+    DATE_RE = re.compile(r"\d\d-[a-z][a-z][a-z]-\d\d|date=|[12]\d\d\d-[01]\d-[0-3]\d")
+
+    # weights for words in URL path (lower is better)
+    # for larger values of max_results, weighting probably
+    # matters less (more sitemaps will be traversed looking
+    # for more matches)
+    PREF_TOKENS: list[tuple[str, float]] = [
+        ("google-news", -2),
+        ("googlenews", -2),
+        ("news", -1),
+        ("xml", -0.5),
+        (".html", DISCARD),
+    ]
+
+    # things in URL path to weight against
+    # (looking for single, whole site index)
+    SECTIONS = [
+        "athlet",  # NYT
+        "authors",  # NYT
+        "best-sell",  # NYT
+        "books",
+        "cities",  # NYT
+        "city",  # NYT
+        "cooking",  # NYT
+        "games",  # NYT
+        "local",
+        "market-data",  # WSJ
+        "recipe",  # NYT
+        "region"  # NYT
+        "sports",  # NYT
+        "teams",  # NYT
+        "video",  # NYT
+        "weather",  # NYT
+    ]
+    SECTION_PREF = DISCARD / 2
+
+    # Preference for canned paths; Since they're not "organic" (seen
+    # in a sitemap from the site), they're slightly preferable to a
+    # URL without ANY matching tokens, but less preferable to a URL
+    # WITH matching tokens:
+    UNPUBLISHED_GNEWS_SITEMAP_PREF = -0.25
+
+    def page_pref(self, url: str) -> float:
+        u = urlsplit(url)  # leaves query params in .path
+
+        # check unflattened path for known paths
+        # of good files:
+        if u.path in discover._UNPUBLISHED_GNEWS_SITEMAP_PATHS:
+            return self.UNPUBLISHED_GNEWS_SITEMAP_PREF
+
+        # check unflattened path for known paths of index files
+        # (prefer index files to urlset files that don't
+        # have any token matches or match a well known path)
+        if u.path in discover._UNPUBLISHED_SITEMAP_INDEX_PATHS:
+            return self.UNPUBLISHED_INDEX_PREF
+
+        path = u.path.lower()
+        if u.query:
+            path += f"?{u.query.lower()}"
+
+        # don't want full year, per month or per-day files!
+        if self.YEAR_RE.search(path) or self.DATE_RE.search(path):
+            return self.DISCARD_PREF
+
+        pref = 0.0
+        for section in self.SECTIONS:
+            if section in path:
+                pref += self.SECTION_PREF
+        if pref >= self.DISCARD_PREF:
+            return pref
+
+        for tok, weight in self.PREF_TOKENS:
+            if tok in path:
+                pref += weight
+        return pref
 
 
 if __name__ == "__main__":
     import sys
 
+    from mcmetadata.webpages import MEDIA_CLOUD_USER_AGENT
+
     logging.basicConfig(level=logging.INFO)
 
+    # http://www.ap.com has a small map, www.npr.org and www.nytimes.com are large!
+    # record so far is univision.com w/ three news sitemaps
+
     if len(sys.argv) != 2:
-        # http://www.ap.com has a small map, www.npr.org and www.nytimes.com are large!
         sys.stderr.write("Usage: venv/bin/python -m mc_sitemap_tools.crawl URL_BASE\n")
         sys.exit(1)
 
-    urls = full_crawl_urlsets(sys.argv[1], 0.1)
-    print("================")
-    for url in urls:
-        print(url)
+    cls = GNewsCrawler  # or GNewsCrawlerFull
+
+    # The first site examined (csmonitor.com) turned out to have a google
+    # news sitemap URL in a sitemap file referenced by robots.txt.
+    crawler = cls(
+        home_page=sys.argv[1],
+        user_agent=MEDIA_CLOUD_USER_AGENT,
+        max_depth=1,
+        max_results=3,
+    )
+
+    sleep_time = 0.1
+    while crawler.visit_one() == VisitResult.MORE:
+        time.sleep(sleep_time)
+    for res in crawler.results:
+        print(res["url"])
